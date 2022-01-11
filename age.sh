5a75bfd7a6412b3563c2a4c069743b71a29315ed
SowTag <sowtag@gmail.com>
2022-01-11 18:12:34 -0300

    Added leap years support
    Just setting years as 365.25 days long instead of 365 days long lmao.
    2 fucking hours trying to add leap year support and here we are.

--- age.sh
+++ age.sh
@@ -5,5 +5,5 @@ now=$(date -d "now" +%s)				# Don't modify this
 
 diff=$(($now - $birth))
 
-echo "$diff/60/60/24/365" | bc -l | awk '{print $1"0000000000000000"}' | awk '{print substr($0, 0, 10)}'
+echo "$diff/60/60/24/365.25" | bc -l | awk '{print $1"0000000000000000"}' | awk '{print substr($0, 0, 10)}'
 # The first awk (which contains the zeroes) is to mantain consistency when numbers get rounded (aka 20.1 years -> 20.100000000).
